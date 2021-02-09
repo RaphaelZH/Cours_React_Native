b13cc8222c2e6eee32cac346339df246a86af27d
haozhang <haozhang@me.com>
2021-02-10 00:36:03 +0100

    update

--- Jour 3/jour-3---td6---fiches-produit/App.js
+++ Jour 3/jour-3---td6---fiches-produit/App.js
@@ -9,10 +9,10 @@ import Fiche from './components/Fiche'
 export default function App() {
 
   const initialFiche = [
-    { isSelected: true, titleOfFiche: "Printemps", pictureURL:"https://chine.in/images/2019/9dc4a98ae7d5a6e0b1956137a61c4fb8.jpg", position: 1}, 
-    { isSelected: false, titleOfFiche: "Été",       pictureURL:"http://5b0988e595225.cdn.sohucs.com/images/20180504/8ab8f8e72af94e1dbf06537414b1f2fe.jpeg", position: 2},
-    { isSelected: false, titleOfFiche: "Automne",   pictureURL:"https://wzh.radiomandarin.fr/uploadfile/2019/0808/20190808081711744.jpg", position: 3},
-    { isSelected: false, titleOfFiche: "Hiver",     pictureURL:"https://i.epochtimes.com/assets/uploads/2019/12/b025b2d1a5af6955e20898a96ca4b1a8-320x200.jpg", position: 4},
+    { titleOfFiche: "Printemps", pictureURL:"https://chine.in/images/2019/9dc4a98ae7d5a6e0b1956137a61c4fb8.jpg", position: 1}, 
+    { titleOfFiche: "Été",       pictureURL:"http://5b0988e595225.cdn.sohucs.com/images/20180504/8ab8f8e72af94e1dbf06537414b1f2fe.jpeg", position: 2},
+    { titleOfFiche: "Automne",   pictureURL:"https://wzh.radiomandarin.fr/uploadfile/2019/0808/20190808081711744.jpg", position: 3},
+    { titleOfFiche: "Hiver",     pictureURL:"https://i.epochtimes.com/assets/uploads/2019/12/b025b2d1a5af6955e20898a96ca4b1a8-320x200.jpg", position: 4},
   ];
   const [table, setTable] = React.useState(initialFiche);
 
